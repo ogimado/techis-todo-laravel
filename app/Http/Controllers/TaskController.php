fbd8607350327f64824fc5ec48bdadc9acb4be07
ogimado <ogimado1112@gmail.com>
2021-12-19 11:46:42 +0900

    TaskController.destroy編集

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -79,8 +79,6 @@ class TaskController extends Controller
 
     public function destroy(Request $request,Task $task)
     {
-        $this->authorize('destroy',$task);
-
         $task->delete();
         return redirect('/tasks');
     }
